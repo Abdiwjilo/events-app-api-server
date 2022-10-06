b440c6eecbcb0622bd32ecf7e84e554fcf23a165
abdiwjilo <abdiwjilo.com>
2022-10-06 16:57:19 +0000

    change team name

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'ABDI JILO';
+    team = 'ABDI W JILO';
     version = '1.0.0';
 }
 module.exports = AppConfig;
